6329ced1e57934a02640d07f8e8dafbb63016ea1
Cody T. Ross <ross.cody.taylor@gmail.com>
2026-07-22 12:48:28 +0200

    Delete grackleator-master directory

--- grackleator-master/R/extract_autocorrelation_parameter.R
+++
@@ -1,9 +0,0 @@
-extract_autocorrelation_parameter = function(m2){
-df2 = data.frame(Parameter="B", Value=rstan::extract(m2,pars="B")$B) # attraction score in (0,1) to 1-lag locations
-
-
- ggplot(df2, aes(x=Value)) + geom_density(aes(fill=Parameter), alpha=0.3) +
- theme(strip.text.x = element_text(size=14,face="bold"),axis.text=element_text(size=12),
-        axis.title=element_text(size=14,face="bold")) 
-}
-
